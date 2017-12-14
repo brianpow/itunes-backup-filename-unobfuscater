37e513b95b397ba229051a9579aaa52a7fe46ae2
Brian Pow <brianpow@gmail.com>
2017-12-15 00:20:49 +0800

    add support to older mbdb format

--- ibfu.py
+++ ibfu.py
@@ -3,32 +3,40 @@
 import sqlite3
 import os
 import argparse
+from mbdb import Mbdb
 
-parser = argparse.ArgumentParser(
-    description='iTunes Backup Filename Unobfuscater. Licence: AGPL-v3.0')
-parser.add_argument('paths', default='.', nargs='*',
-                    help='Path of iTunes Backup [default: current working directory]')
-parser.add_argument('-d', '--dryrun', dest='dryrun',
-                    action='store_true', help='Dry run, don\'t rename any files')
-parser.add_argument('-u', '--undo', dest='undo',
-                    action='store_true', help='Undo rename')
-parser.add_argument('-v', '--verbose', dest='verbose',
-                    action='count', help='Be more verbose')
-parser.add_argument(
-    '-V',
-    '--version',
-    action='version',
-    version='%(prog)s 1.0')
 
-args = parser.parse_args()
+def process_mbdb(filename):
+    db = Mbdb(filename)
+    mbdb = db.mbdb
+    for index, row in mbdb.items():
+        if (row['mode'] & 0xE000) != 0x4000:
+            if args.undo:
+                dst = os.sep.join([path, row['fileID']])
+                src = os.sep.join(
+                    [path, row['domain'], row['filename'].replace('/', os.sep)])
+            else:
+                src = os.sep.join([path, row['fileID']])
+                dst = os.sep.join(
+                    [path, row['domain'], row['filename'].replace('/', os.sep)])
+            if args.verbose:
+                print "Renaming %s to %s" % (src, dst)
+            try:
+                if not args.dryrun:
+                    os.renames(src, dst)
+            except OSError:
+                print "Error occured when renaming %s to %s" % (src, dst)
+        elif row['filename'] != '':
+            if args.verbose:
+                print "Skipping root directory %s (%s)" % (row['domain'], row['fileID'])
+        else:
+            if args.verbose:
+                print "Skipping directory %s (%s)" % (os.sep.join([row['domain'], row['filename']]), row['fileID'])
 
-for path in args.paths:
-    db = os.sep.join([path, 'Manifest.db'])
-    if not os.path.isfile(db):
-        print "Unable to locate database file at %s" % (db)
-        continue
-    print "Processing %s..." % (db)
-    conn = sqlite3.connect(db)
+
+def process_db(filename):
+    print "Processing %s..." % (filename)
+    conn = sqlite3.connect(filename)
 
     c = conn.cursor()
     for row in c.execute('SELECT * FROM Files'):
@@ -46,3 +54,33 @@ for path in args.paths:
                     os.renames(src, dst)
             except OSError:
                 print "Error occured when renaming %s to %s" % (src, dst)
+
+
+parser = argparse.ArgumentParser(
+    description='iTunes Backup Filename Unobfuscater. Licence: AGPL-v3.0')
+parser.add_argument('paths', default='.', nargs='*',
+                    help='Path of iTunes Backup [default: current working directory]')
+parser.add_argument('-d', '--dryrun', dest='dryrun',
+                    action='store_true', help='Dry run, don\'t rename any files')
+parser.add_argument('-u', '--undo', dest='undo',
+                    action='store_true', help='Undo rename')
+parser.add_argument('-v', '--verbose', dest='verbose',
+                    action='count', help='Be more verbose')
+parser.add_argument(
+    '-V',
+    '--version',
+    action='version',
+    version='%(prog)s 1.1')
+
+args = parser.parse_args()
+
+for path in args.paths:
+    db = os.sep.join([path, 'Manifest.db'])
+    db2 = os.sep.join([path, 'Manifest.mbdb'])
+    if os.path.isfile(db):
+        process_db(db)
+    elif os.path.isfile(db2):
+        process_mbdb(db2)
+    else:
+        print "Unable to locate database file at %s or %s" % (db, db2)
+        continue
